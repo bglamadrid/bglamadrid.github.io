fffd2e409692de099a9023efe5ceb2f855cb4a96
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-06-28 02:05:51 -0400

    build(webpack): add dev config file

---
+++ webpack.config.dev.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const PugPlugin = require('pug-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  entry: {
+    index: './src/views/index.pug'
+  },
+  output: {
+    filename: 'scripts/[name].[contenthash:8].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    clean: true
+  },
+  plugins: [
+    new PugPlugin({
+      modules: [
+        PugPlugin.extractCss({
+          filename: 'styles/[name].[contenthash:8].css'
+        })
+      ]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: PugPlugin.loader,
+        options: {
+          method: 'render'
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: ['css-loader']
+      },
+      {
+        test: /favicon\.ico/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'favicon.ico'
+        }
+      }
+    ]
+  }
+};
