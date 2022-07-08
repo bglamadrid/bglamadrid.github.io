66853a09eb159f603279f738ef775173d3cec216
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-07-08 02:54:43 -0400

    refactor(pug/tailwindcss): migrate remaining css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: {
@@ -11,5 +13,16 @@ module.exports = {
       sans: ['Ubuntu Mono', 'sans-serif'],
     },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addBase, addUtilities, theme }) {
+      addBase({
+        '*': { textAlign: 'inherit' },
+        'body': { textAlign: 'center' },
+        'h1': { fontSize: theme('fontSize.4xl') },
+        'h2': { fontSize: theme('fontSize.2xl') },
+        'h3': { fontSize: theme('fontSize.xl') },
+        'h4': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ],
+};
