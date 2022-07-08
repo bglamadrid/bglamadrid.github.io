7a53b8c0b739b889a254a62e263120deaa80223b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-07-08 03:15:24 -0400

    build(webpack): simpler dev config file

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,53 +1,12 @@
-const path = require('path');
-const PugPlugin = require('pug-plugin');
+const config = require('./webpack.config');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  entry: {
-    index: './src/views/index.pug'
-  },
-  output: {
-    filename: 'scripts/[name].[contenthash:8].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    clean: true
-  },
-  plugins: [
-    new PugPlugin({
-      modules: [
-        PugPlugin.extractCss({
-          filename: 'styles/[name].[contenthash:8].css'
-        })
-      ]
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: PugPlugin.loader,
-        options: {
-          method: 'render'
-        }
-      },
-      {
-        test: /\.(css)$/,
-        use: [
-          'css-loader',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /favicon\.ico/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'favicon.ico'
-        }
-      }
-    ]
+module.exports = Object.assign(
+  config,
+  {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    }
   }
-};
+);
